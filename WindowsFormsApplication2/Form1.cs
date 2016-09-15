966abf0a50f52eb7a366ad0eef6d8c2ebd6c64ac
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2016-09-15 01:16:42 -0500

    Agregando cajas de texto.
    Adding text boxes.

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -26,17 +26,27 @@ namespace WindowsFormsApplication2
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {           
+
+        }
+
+        /**
+         * Este es el método del botón. This is the button method.
+         * Todo el código se ha colocado aquí para que se pueda controlar por el usuario.
+         * All code is place hee because it can be controled by user.
+         */
+        private void button1_Click(object sender, EventArgs e)
         {
             string datosConexion = "Data Source=localhost;Initial Catalog=bd_prueba_sqlserver;Integrated Security=true;";
             SqlConnection con = new SqlConnection(datosConexion);
             //con.ConnectionString = datosConexion;
             //con.Open();
 
-            SqlCommand command = new SqlCommand( "INSERT INTO tb_usuarios ( documento, nobre ) VALUES( '12', 'Camilo F' )", con);
+            //En este punto, se convoca a los controles de texto. Ya ingresa datos.
+            //In this point, the text boxes is put here. Now we can write data.
+            SqlCommand command = new SqlCommand("INSERT INTO tb_usuarios ( documento, nobre ) VALUES( '" + this.textBox1.Text + "', '" + this.textBox2.Text + "' )", con);
             command.Connection.Open();
             command.ExecuteNonQuery();
-            
-
         }
     }
 }
